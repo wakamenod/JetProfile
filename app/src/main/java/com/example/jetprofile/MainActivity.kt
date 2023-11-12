f370a4b320ca7db7a47e13fda03ab8ddb0fecf86
wakamenod <wakamenod@gmail.com>
2023-11-12 14:30:08 +0900

    mod

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.jetprofile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -26,6 +30,11 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(painter = painterResource(id = R.drawable.kids_kenka),
+                              contentDescription = "Kids_Kenka",
+                              modifier = Modifier.size(100.dp).clip()
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
                         Text(
                             text = "aaa",
                             color = Color.Gray,
@@ -34,8 +43,8 @@ class MainActivity : ComponentActivity() {
                         )
                         Spacer(modifier = Modifier.height(20.dp))
                         Text(
-                            text = "職業: Android Engineer",
-                            color = Color.Gray,
+                            text = "職業: Android Engineer あああ",
+                            color = Color.Green,
                             fontSize = 16.sp,
                         )
                     }
